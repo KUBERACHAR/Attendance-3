821ab8dcb453fc3f721731125916bc82bf1d06aa
KUBERACHAR <kuberachr01@gmail.com>
2026-08-05 17:44:52 +0530

    Fixed the chunk size warning limit to 1MB in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the limit to 1000 kB (1 MB) or higher
+    chunkSizeWarningLimit: 1000,
+  },
 });
